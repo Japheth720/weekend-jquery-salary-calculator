646c5d25918364fdf844e2e30ee85b760d61124c
Japheth Yang <japheth720@gmail.com>
2023-04-21 15:34:18 -0500

    fixed and finished main code

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 $(document).ready(onReady);
 
+let totalAnnualSalary = 0;
+
 function onReady() {
     console.log('testing!!');
     $('#submit-button').on('click', onSubmit);
@@ -7,30 +9,56 @@ function onReady() {
 
 }
 
-
-function onSubmit() {
+let currentYearlySalaryNum = 0;
+function onSubmit(event) {
     event.preventDefault();
-    let firstNameInput = $('#firstname-input').val('');
-    let lastNameInput = $('#lastname-input').val('');
-    let idInput = $('#id-input').val('');
-    let titleInput = $('#title-input').val('');
-    let salaryInput = $('#salary-input').val('');
+    let firstNameInput = $('#firstname-input').val();
+    let lastNameInput = $('#lastname-input').val();
+    let idInput = $('#id-input').val();
+    let titleInput = $('#title-input').val();
+    let salaryInput = $('#salary-input').val();
 
+    let salaryInputNum = Number(salaryInput);
 
-    $('#tbody').append(`
-    <tr>
-      <td>${firstNameInput}</td>
-      <td>${lastNameInput}</td>
-      <td>${idInput}</td>
-      <td>${titleInput}</td>
-      <td>${salaryInput}</td>
-      <td>
-        <button class='deleteBtn'>Delete</button>
-      </td>
-    </tr>
-    `);
+  console.log(firstNameInput);
+  console.log(salaryInputNum);
+
+  $('#tbody').append(`
+  <tr>
+    <td>${firstNameInput}</td>
+    <td>${lastNameInput}</td>
+    <td>${idInput}</td>
+    <td>${titleInput}</td>
+    <td>${toDollarString(salaryInputNum)}</td>
+    <td>
+      <button class='deleteBtn'>Delete</button>
+    </td>
+  </tr>
+  `);  
+
+  currentYearlySalaryNum += salaryInputNum;
+  
+  let monthlySalary = currentYearlySalaryNum / 12;
+  
+  console.log(monthlySalary);
+
+  $('#monthly-salary-total').text(monthlySalary);
+  
+  if (monthlySalary >= 20000) {
+    
+    $('#monthly-salary-total').addClass('over-budget-background')
+  }
 }
 
 function onDelete() {
- $(this).closest('tr').remove();
+  $(this).closest('tr').remove();
+}
+
+
+function toDollarString(dollarsAsNumber) {
+  let dollarString = new Intl.NumberFormat(
+    'en-US', 
+    { style: 'currency', currency: 'USD' }
+  ).format(dollarsAsNumber);
+  return dollarString;
 }
